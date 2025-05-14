4c1c7e09f1676627765640b10a6fc2b1ded0840a
Miguel Barbosa Pereira <miguel.b.per@gmail.com>
2025-05-14 21:55:02 +0100

    Add simple train config

--- src/lightning_hydra_zen_template/classical/configs/train.py
+++ src/lightning_hydra_zen_template/classical/configs/train.py
@@ -1,7 +1,22 @@
-from hydra_zen import make_config
+from hydra_zen import make_config, make_custom_builds_fn
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+from lightning_hydra_zen_template.classical.core.model import Model
+from lightning_hydra_zen_template.classical.core.trainer import Trainer
+from lightning_hydra_zen_template.classical.data.iris import IrisDataModule
+
+fbuilds = make_custom_builds_fn(populate_full_signature=True)
 
 TrainCfg = make_config(
-    data=None,
-    model=None,
-    trainer=None,
+    data=fbuilds(IrisDataModule),
+    model=fbuilds(
+        Model,
+        model=fbuilds(LogisticRegression),
+        metrics=[
+            fbuilds(accuracy_score),
+        ],
+    ),
+    trainer=fbuilds(Trainer),
+    monitor="val/accuracy_score",
 )
